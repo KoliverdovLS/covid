71d141ecfb33821c0f7e163714909c0e0a95f89c
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-12-17 17:36:08 +0300

    added some transperency

--- app/components/menu.js
+++ app/components/menu.js
@@ -2,15 +2,19 @@ import '../styles/menu.css';
 
 export default function showPopUpMenu(context) {
   const container = document.createElement('div');
-  container.className = ('menu-container container-fluid justify-content-between align-items-center');
+  container.className = ('menu-container container-fluid p-0 justify-content-between align-items-center');
 
   const columnFirst = document.createElement('div');
   const columnSecond = document.createElement('div');
   const columnThird = document.createElement('div');
 
-  columnFirst.className = 'first-column col-4';
-  columnSecond.className = 'first-column col-4';
-  columnThird.className = 'first-column col-4';
+  columnFirst.className = 'first-column m-0 p-0 col-2 fs-6';
+  columnSecond.className = 'second-column m-0 p-0 col-5 fs-6';
+  columnThird.className = 'third-column m-0 p-0 col-5 fs-6';
+
+  columnFirst.textContent = 'Widen';
+  columnSecond.textContent = 'Option 1/2';
+  columnThird.textContent = 'Option 3/4';
 
   container.appendChild(columnFirst);
   container.appendChild(columnSecond);
