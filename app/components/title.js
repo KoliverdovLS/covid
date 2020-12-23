15a4162c44a5cc1e35f201bfc60235e88eb22756
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-12-23 17:00:28 +0300

    updated css

--- app/components/title.js
+++ app/components/title.js
@@ -4,11 +4,13 @@ export default function title(context) {
   const container = document.createElement('div');
   const row = document.createElement('div');
   const column = document.createElement('div');
-  container.className = ('container-fluid');
-  row.className = ('row justify-content-center align-items-center');
-  column.className = ('col h3');
-  column.textContent = 'COVID Dashboard';
-  container.appendChild(row);
-  row.appendChild(column);
+  container.className = ('title-inner-container container-fluid m-0 p-1 justify-content-center align-items-center');
+  // row.className = ('row justify-content-center align-items-center h3');
+  // column.className = ('col h3');
+  // column.textContent = 'COVID Dashboard';
+  // container.appendChild(row);
+  // row.appendChild(column);
+  container.textContent = 'COVID Dashboard';
+
   return container;
 }
