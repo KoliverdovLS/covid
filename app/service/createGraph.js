22b29e63d28928a96bb51d6e566bdf3db601ffc2
Daria Sukhova <d.es.tiny.1234545@gmail.com>
2020-12-26 10:46:37 +0300

    feat: fix graphics

--- app/service/createGraph.js
+++ app/service/createGraph.js
@@ -1,35 +1,49 @@
 import 'chartjs-plugin-zoom/dist/chartjs-plugin-zoom';
 import 'moment/dist/moment';
+let title, colour;
+
+function setTitleAndColour(context) {
+  if (context.dataToShow === 'overall') {
+    title = 'Overall';
+    colour = '255,165,0';
+  }
+  else   if (context.dataToShow === 'deaths') {
+    title = 'Deaths';
+    colour = '255, 0, 0';
+  }
+  if (context.dataToShow === 'recov') {
+    title = "Recoveries";
+    colour = '0,128,0';
+  }
+}
 
 export function setOptionsForgraphics(context, days, data) {
-  console.log('context!', context);
+  setTitleAndColour(context);
   let timeFormat = 'MM/DD/YYYY HH:mm';
   const options = {
     type: 'bar',
     data: {
         labels: days,
         datasets: [{
-            label: context.destination || 'In the World',
+            label: title,
             data: data,
-            backgroundColor: 'rgba(255, 99, 132, 0.8)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            fontColor: 'rgba(255, 255, 255, 1)',
+            backgroundColor: `rgba(${colour}, 0.8)`,
+            borderColor: `rgba(${colour}, 1)`,
             borderWidth: 1,
         }]
     },
     options: {
       responsive: true,
+      maintainAspectRatio: false,
       title: {
         display: true,
-        text: context.dataToShow || 'Confirmed Cases',
-        fontColor: 'rgba(255,255,255,1)',
+        text: context.destination || 'In the World',
       },
       scales: {
         x: {
           type: 'time',
           time: {
             format: timeFormat,
-            // round: 'day'
             tooltipFormat: 'll HH:mm'
           },
           scaleLabel: {
@@ -61,7 +75,7 @@ export function setOptionsForgraphics(context, days, data) {
           enabled: true,
           drag: false,
           mode: 'x',
-          speed: 0.0001,
+          speed: 0.000001,
           threshold: 0,
           sensitivity: 0
         }
